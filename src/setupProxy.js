bc814b7764ac2e76f388ea39d2ee0e4bb2fcd408
sriramvuddagiri <111992308+sriramvuddagiri@users.noreply.github.com>
2022-10-21 11:14:52 +0530

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/app1', {
-      target: 'http://localhost:8082', // API endpoint 1
-    //   target: 'http://3.87.108.193:8082',
+      //target: 'http://localhost:8082', // API endpoint 1
+     target: 'http://shoppingappuserservice-env.eba-bgrrdm7h.us-east-2.elasticbeanstalk.com',
       changeOrigin: true,
       pathRewrite: {
         "^/app1": "",
@@ -16,8 +16,8 @@ module.exports = function (app) {
   );
   app.use(
     createProxyMiddleware('/app2', {
-      target: 'http://localhost:8083', // API endpoint 2
-    //   target: 'http://34.229.167.216:8083',
+      //target: 'http://localhost:8083', // API endpoint 2
+     target: 'http://shoppingappproductservice-env.eba-pumkxnme.us-east-2.elasticbeanstalk.com',
       changeOrigin: true,
       pathRewrite: {
         "^/app2": "",
